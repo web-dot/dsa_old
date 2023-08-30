2b7bd52f0822b9a4b6c729eedf59038cb90042bd
sudipto <cg.sudipc@gmail.com>
2023-08-30 09:30:13 +0530

    insertion sort recap

---
+++ src/com/dsa/sortng/SortingRecap.java
@@ -0,0 +1,26 @@
+package com.dsa.sortng;
+
+import java.util.Arrays;
+
+public class SortingRecap {
+	
+	public static int[] insertionSort(int[] arr) {
+		for(int i=0; i<arr.length; i++) {
+			int sorted = arr[i];
+			int j = i-1;
+			while(j>=0 && arr[j] > sorted ) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j+1] = sorted;
+		}
+		return arr;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {4, 3, 1, 2};
+		int[] out = insertionSort(arr);
+		System.out.println(Arrays.toString(out));
+	}
+
+}
