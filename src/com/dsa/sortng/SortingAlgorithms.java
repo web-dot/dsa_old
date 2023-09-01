0bb148d5169695b2b68b0a3cf385366f18fba589
sudipto <cg.sudipc@gmail.com>
2023-09-01 22:54:32 +0530

    understanding selection sort - checking pseudocode

--- src/com/dsa/sortng/SortingAlgorithms.java
+++ src/com/dsa/sortng/SortingAlgorithms.java
@@ -20,20 +20,25 @@ public class SortingAlgorithms {
 	private static int[] selectionSort(int[] arr) {
 		for(int i=0; i<arr.length-1; i++) {
 			int min = i;
+			System.out.println("min =" + arr[min]);
+			System.out.println("arr[i] =" + arr[i]);
 			for(int j=i+1; j<arr.length; j++) {
+				System.out.println("arr[j] =" + arr[j]);
 				if(arr[j] < arr[min]) {
 					min = j;
+					System.out.println("new min =" + arr[min]);
 				}
 			}
-			int temp = arr[i];
-			arr[i] = arr[min];
-			arr[min] = temp;
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+			System.out.println(Arrays.toString(arr));
 		}
 		return arr;
 	}
 	
 	public static void main(String[] args) {
-		int[] arr = {4, 3, 1, 2};
+		int[] arr = {44,10,46,11,26,36,29,48,25,43,39};
 		System.out.println(Arrays.toString(selectionSort(arr)));
 	}
 
