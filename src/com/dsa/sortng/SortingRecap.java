046c15fc0210cea3a2d1024beb51d13eba885490
sudipto <cg.sudipc@gmail.com>
2023-09-04 00:01:53 +0530

    selection sort recap

--- src/com/dsa/sortng/SortingRecap.java
+++ src/com/dsa/sortng/SortingRecap.java
@@ -19,14 +19,24 @@ public class SortingRecap {
 	
 	
 	public static int[] selectionSort(int[] arr) {
-		
-		return null;
+		for(int i=0; i<arr.length; i++) {
+			int min = i;
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			int temp = arr[min];
+			arr[min] = arr[i];
+			arr[i] = temp;
+		}
+		return arr;
 	}
 	
 
 	public static void main(String[] args) {
 		int[] arr = {4, 3, 1, 2};
-		int[] out = insertionSort(arr);
+		int[] out = selectionSort(arr);
 		System.out.println(Arrays.toString(out));
 	}
 
