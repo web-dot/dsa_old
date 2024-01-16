44f0fb7c8a718c1a7d236e62b542a8e76922254d
sudipto <cg.sudipc@gmail.com>
2024-01-16 08:39:52 +0530

    binary search - logn

--- src/com/dsa/array/ArrayOperations.java
+++ src/com/dsa/array/ArrayOperations.java
@@ -19,11 +19,34 @@ public class ArrayOperations {
 		nums[position] = value;
 		return nums;
 	}
+	
+	
+	public static int binarySearch(int[] arr, int item) {
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		int start = 0;
+		int end = arr.length - 1;
+		while(start < end) {
+			int mid = start + (end - start) / 2;
+			System.out.println("mid = " + arr[mid]);
+			if(item == arr[mid]) {
+				return mid;
+			}
+			else if(item < arr[mid]) {
+				end = mid - 1;
+			}
+			else {
+				start = mid + 1;
+			}
+		}
+		return -1;
+	}
 
 	public static void main(String[] args) {
-
-		int[] nums = new int[] { 2 };
-		System.out.println(Arrays.toString(shiftArray(nums, 0, 1)));
+		int[] arr = {3,2,1,4,5};
+		System.out.println(binarySearch(arr, 4));
+//		int[] nums = new int[] { 2 };
+//		System.out.println(Arrays.toString(shiftArray(nums, 0, 1)));
 
 	}
 }
